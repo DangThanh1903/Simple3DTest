e098cd3be3b79faa12375549c94f7049d2539f68
Dang Thanh <124600706+DangThanh1903@users.noreply.github.com>
2026-05-04 22:37:17 +0700

    Initial Unity project

---
+++ Assets/Scripts/CameraTargetSingleton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace TMG.Survivors
+{
+    public class CameraTargetSingleton : MonoBehaviour
+    {
+        public static CameraTargetSingleton Instance;
+
+        public void Awake()
+        {
+            if (Instance != null)
+            {
+                Debug.LogWarning("Warning multiple instances of CameraTargetSingleton detected. Destroying new instance.", Instance);
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = this;
+        }
+    }
+}
